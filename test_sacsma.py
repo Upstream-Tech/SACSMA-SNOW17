8d7f8b68212fe8223282e280f191413cc70d0319
Grey Nearing <grey@upstream.tech>
2020-04-13 10:46:53 -0500

    Added all SAC-SMA funcitons, including fortran/python interface.

---
+++ test_sacsma.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import sacsma_utilities
+import camels_utilities
+import pet
+import matplotlib.pyplot as plt
+
+# Location of camels data
+camels_dir = '/Users/grey/workspace/camels_data/'
+
+# Which forcings
+forcing_type = 'nldas'
+#forcings = 'maurer'
+#forcings = 'daymet'
+
+# Gauge ID to test
+gauge_id = '09378170'
+
+# Load all necessary data
+parameters = camels_utilities.load_sacsma_parameters(camels_dir, forcing_type, gauge_id)
+forcings = camels_utilities.load_forcings(camels_dir, forcing_type, gauge_id)
+attributes = camels_utilities.load_basin_attributes(camels_dir, gauge_id)
+benchmark = camels_utilities.load_discharge(camels_dir, forcing_type, gauge_id)
+dates = forcings['Date']
+
+# Calculate potential evaporation
+forcings['PET(mm/day)'] = pet.get_priestley_taylor_pet(forcings['Tmin(C)'], forcings['Tmax(C)'], forcings['SRAD(W/m2)'], 
+                                                       attributes['gauge_lat'], attributes['elev_mean'], dates.dt.dayofyear)
+
+# Run SAC-SMA
+outputs, states = sacsma_utilities.run_sacsma(dates, forcings, parameters)
+
+# Plot results
+plt.plot(outputs['surf'] + outputs['grnd'], label='Our SAC-SMA')
+plt.plot(benchmark['OBS_RUN'], label='Observations')
+plt.plot(benchmark['MOD_RUN'], label='NCAR SAC-SMA')
+plt.grid()
+plt.show()
+
+import pdb
+pdb.set_trace()
